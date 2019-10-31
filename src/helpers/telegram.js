9aa1d87df2b02d49feeab3270ffd2867aee37ecd
Florian Becker <fb@vxapps.com>
2019-10-31 04:26:49 +0100

    Sending message even if sticker throws exception

--- src/helpers/telegram.js
+++ src/helpers/telegram.js
@@ -82,6 +82,16 @@ process.on('message', (msg) => {
     			})
 	    	}).catch((err) => {
 		    	log.error(`Failed sending Telegram sticker to ${msg.job.name}. Error: ${err.message}`)
+                
+                client.telegram.sendMessage(msg.job.target, message, { parse_mode: 'Markdown', disable_web_page_preview: true }).then(() => {
+			    if (config.telegram.location || (message.type === 'pokemon' && config.telegram.pokemon_location) || (message.type === 'raid' && config.telegram.raid_location) || (message.type === 'quest' && config.telegram.quest_location)) {
+				    client.telegram.sendLocation(msg.job.target, msg.job.lat, msg.job.lon, { disable_notification: true }).catch((err) => {
+					    log.error(`Failed sending Telegram Location to ${msg.job.name}. Error: ${err.message}`)
+    			    	})
+    		    	}
+	        	}).catch((err) => {
+	    	    	log.error(`Failed sending Telegram message to ${msg.job.name}. Error: ${err.message}`)
+        		})
     		})
         }
         else {
